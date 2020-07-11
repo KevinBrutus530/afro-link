98dca8a23aded7c2fd205deacc38aebba768d68b
maria liza <mariamartinez@pursuit.org>
2020-07-11 12:00:49 -0400

    update seeds file with home decor

---
+++ backend/db/seeds.sql
@@ -0,0 +1,162 @@
+
+\c afro_link;
+
+INSERT INTO businesses(biz_name,hours) VALUES ('228 Grant Street Candle Co.','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('AphroChic','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('The Black Home','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Black Pepper Paperie Co.','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Bolé Road Textiles','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Clare','Mon - Fri, 9am - 9pm 
+Sat - Sun, 9am - 5pm');
+INSERT INTO businesses(biz_name,hours) VALUES ('Claude Home','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Effortless Composition','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Goodee','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Ilé Ilà','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Jungalow','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Lichen','Sat - Sun, 12pm - 5pm
+Mon, Closed or By Appointment
+Tue, Closed or By Appointment
+Wed, 12pm - 5pm
+Thur, Closed or By Appointment
+Fri, Closed or By Appointment');
+INSERT INTO businesses(biz_name,hours) VALUES ('Linoto','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Lit Bklyn','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Malene B','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Marie Burgos Collection','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Natty Garden','All Week, 10am - 6pm');
+INSERT INTO businesses(biz_name,hours) VALUES ('Peace & Riot','Tue - Fri, 11am - 6pm  
+Sat, 10am - 7pm  
+Sun, 11am - 6pm');
+INSERT INTO businesses(biz_name,hours) VALUES ('Pottery by Osa','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Robin Wilson Home','Available during weekday business hours, but we are typically off-site focused on client projects');
+INSERT INTO businesses(biz_name,hours) VALUES ('Rochelle Porter','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('See Line Ceramics','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Sheila Bridges','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('SustainAble Home Goods','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Tackussanu Senegal','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Tactile Matter','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('xN Studio','online business');
+INSERT INTO businesses(biz_name,hours) VALUES ('Yowie','online business');
+
+
+INSERT INTO owners(owner_id,owner_name) VALUES (1,NULL);
+INSERT INTO owners(owner_id,owner_name) VALUES (2,'Jeanine Hays and Bryan Mason');
+INSERT INTO owners(owner_id,owner_name) VALUES (3,'Neffi Walker');
+INSERT INTO owners(owner_id,owner_name) VALUES (4,'Hadiya Williams');
+INSERT INTO owners(owner_id,owner_name) VALUES (5,'Hana Getachew');
+INSERT INTO owners(owner_id,owner_name) VALUES (6,'Nicole Gibbons');
+INSERT INTO owners(owner_id,owner_name) VALUES (7,NULL);
+INSERT INTO owners(owner_id,owner_name) VALUES (8,'Brittiny Terry');
+INSERT INTO owners(owner_id,owner_name) VALUES (9,'Byron and Dexter Peart');
+INSERT INTO owners(owner_id,owner_name) VALUES (10,'Tosin Oshinowo');
+INSERT INTO owners(owner_id,owner_name) VALUES (11,'Justina Blakeney');
+INSERT INTO owners(owner_id,owner_name) VALUES (12,'Ed Be & Jared Blake');
+INSERT INTO owners(owner_id,owner_name) VALUES (13,'Jason Evege');
+INSERT INTO owners(owner_id,owner_name) VALUES (14,'Denequa Williams');
+INSERT INTO owners(owner_id,owner_name) VALUES (15,'Malene Barnett');
+INSERT INTO owners(owner_id,owner_name) VALUES (16,'Marie Burgos');
+INSERT INTO owners(owner_id,owner_name) VALUES (17,'Joel Mahfood');
+INSERT INTO owners(owner_id,owner_name) VALUES (18,'Achuziam Maha-Sanchez');
+INSERT INTO owners(owner_id,owner_name) VALUES (19,'Osa Atoe');
+INSERT INTO owners(owner_id,owner_name) VALUES (20,'Robin Wilson');
+INSERT INTO owners(owner_id,owner_name) VALUES (21,'Rochelle Porter');
+INSERT INTO owners(owner_id,owner_name) VALUES (22,'Nikita Richardson');
+INSERT INTO owners(owner_id,owner_name) VALUES (23,'Sheila Bridges');
+INSERT INTO owners(owner_id,owner_name) VALUES (24,'LaToya Tucciarone');
+INSERT INTO owners(owner_id,owner_name) VALUES (25,NULL);
+INSERT INTO owners(owner_id,owner_name) VALUES (26,'Kenesha Sneed');
+INSERT INTO owners(owner_id,owner_name) VALUES (27,'Nasozi Kakembo');
+INSERT INTO owners(owner_id,owner_name) VALUES (28,'Shannon Maldonado');
+
+INSERT INTO types(type_name) VALUES ("home decor");
+
+INSERT INTO categories(biz_id,type_id) VALUES (1,1);
+INSERT INTO categories(biz_id,type_id) VALUES (2,1);
+INSERT INTO categories(biz_id,type_id) VALUES (3,1);
+INSERT INTO categories(biz_id,type_id) VALUES (4,1);
+INSERT INTO categories(biz_id,type_id) VALUES (5,1);
+INSERT INTO categories(biz_id,type_id) VALUES (6,1);
+INSERT INTO categories(biz_id,type_id) VALUES (7,1);
+INSERT INTO categories(biz_id,type_id) VALUES (8,1);
+INSERT INTO categories(biz_id,type_id) VALUES (9,1);
+INSERT INTO categories(biz_id,type_id) VALUES (10,1);
+INSERT INTO categories(biz_id,type_id) VALUES (11,1);
+INSERT INTO categories(biz_id,type_id) VALUES (12,1);
+INSERT INTO categories(biz_id,type_id) VALUES (13,1);
+INSERT INTO categories(biz_id,type_id) VALUES (14,1);
+INSERT INTO categories(biz_id,type_id) VALUES (15,1);
+INSERT INTO categories(biz_id,type_id) VALUES (16,1);
+INSERT INTO categories(biz_id,type_id) VALUES (17,1);
+INSERT INTO categories(biz_id,type_id) VALUES (18,1);
+INSERT INTO categories(biz_id,type_id) VALUES (19,1);
+INSERT INTO categories(biz_id,type_id) VALUES (20,1);
+INSERT INTO categories(biz_id,type_id) VALUES (21,1);
+INSERT INTO categories(biz_id,type_id) VALUES (22,1);
+INSERT INTO categories(biz_id,type_id) VALUES (23,1);
+INSERT INTO categories(biz_id,type_id) VALUES (24,1);
+INSERT INTO categories(biz_id,type_id) VALUES (25,1);
+INSERT INTO categories(biz_id,type_id) VALUES (26,1);
+INSERT INTO categories(biz_id,type_id) VALUES (27,1);
+INSERT INTO categories(biz_id,type_id) VALUES (28,1);
+
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (1,NULL,NULL,'https://www.instagram.com/228grantstreet/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (2,NULL,NULL,'https://www.instagram.com/aphrochic/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (3,NULL,NULL,'https://www.instagram.com/theblackhomedesign/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (4,NULL,NULL,'https://www.instagram.com/blackpepperpaperieco/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (5,6468203794,NULL,'https://www.instagram.com/boleroadtextiles/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (6,NULL,NULL,'https://www.instagram.com/clarepaint/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (7,NULL,NULL,'https://www.instagram.com/claudehome/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (8,NULL,NULL,'https://www.instagram.com/effortlesscomposition/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (9,NULL,NULL,'https://www.instagram.com/goodeeworld/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (10,NULL,NULL,'https://www.instagram.com/ile.ila/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (11,NULL,NULL,'https://www.instagram.com/thejungalow/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (12,NULL,NULL,'https://www.instagram.com/lichennyc/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (13,8889546686,NULL,'https://www.instagram.com/linotodotcom/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (14,NULL,NULL,'https://www.instagram.com/litbklyn/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (15,NULL,NULL,'https://www.instagram.com/maleneblifestyle/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (16,9173539149,NULL,'https://www.instagram.com/marieburgos.design/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (17,7184838833,NULL,'https://www.instagram.com/nattygarden/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (18,NULL,NULL,'https://www.instagram.com/peaceandriot/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (19,NULL,NULL,'https://www.instagram.com/potterybyosa/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (20,8008303874,NULL,'https://www.instagram.com/robinwilsonhome/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (21,NULL,NULL,'https://www.instagram.com/roporterdesign/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (22,NULL,NULL,'https://www.instagram.com/seelineceramics/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (23,2126786872,NULL,'https://www.instagram.com/harlemtoilegirl/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (24,NULL,NULL,'https://www.instagram.com/sustainablehomegoods/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (25,7707276794,NULL,'https://www.instagram.com/tackussanu_senegal/');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (26,NULL,NULL,'https://www.instagram.com/tactilematter/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (27,NULL,NULL,'https://www.instagram.com/xnstudio_/?hl=en');
+INSERT INTO contacts(contact_id,phone,email,social_media) VALUES (28,NULL,NULL,'https://www.instagram.com/helloyowie/');
+
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (1,NULL,NULL,NULL,NULL,'https://www.228grantstreet.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (2,NULL,NULL,NULL,NULL,'https://www.aphrochic.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (3,'623 Eagle Rock Ave Suite 124','West Orange','NJ',7052,'https://www.theblackhome.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (4,NULL,NULL,NULL,NULL,'https://shopbppco.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (5,NULL,NULL,NULL,NULL,'https://boleroadtextiles.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (6,NULL,NULL,NULL,NULL,'https://www.clare.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (7,NULL,NULL,NULL,NULL,'https://www.claudehome.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (8,NULL,NULL,NULL,NULL,'https://effortlesscomposition.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (9,NULL,NULL,NULL,NULL,'https://www.goodeeworld.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (10,NULL,NULL,NULL,NULL,'https://www.ile-ila.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (11,NULL,NULL,NULL,NULL,'https://www.jungalow.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (12,'98 Moore Street','Brooklyn','NY',11206,'https://www.lichennyc.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (13,'3199 Albany Post Rd, Suite 215','Buchanan','NY',10511,'https://www.linoto.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (14,NULL,NULL,NULL,NULL,'https://www.litbklyn.co/get-lit');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (15,NULL,NULL,NULL,NULL,'https://malenebarnett.com/shop');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (16,NULL,NULL,NULL,NULL,'https://www.marieburgosdesignthestore.com/marie-burgos-collection-modern-furniture-home-decor');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (17,'636 Washington Ave','Brooklyn','NY',11238,'https://www.nattygarden.com/all-products');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (18,NULL,NULL,NULL,NULL,'http://www.peaceandriot.com/shop');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (19,NULL,NULL,NULL,NULL,'https://www.potterybyosa.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (20,'230 Park Avenue, 3rd fl-WEST','New York','NY',10169,'https://robinwilsonhome.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (21,NULL,NULL,NULL,NULL,'http://www.rochelleporter.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (22,NULL,NULL,NULL,NULL,'https://seelineceramics.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (23,NULL,NULL,NULL,NULL,'https://www.sheilabridges.com/shop/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (24,NULL,NULL,NULL,NULL,'https://yoursustainablehome.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (25,'675 Ponce De Leon Ave NE N215','Atlanta','GA',30308,'https://tackussanusenegal.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (26,NULL,NULL,NULL,NULL,'https://www.tactilematter.com/shop');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (27,NULL,NULL,NULL,NULL,'http://www.osxnasozi.com/');
+INSERT INTO addresses(address_id,street,city,state,zip,website) VALUES (28,NULL,NULL,NULL,NULL,'https://www.shopyowie.com/');
+
+INSERT INTO reviews (review_id, text, name, ratings, zip) VALUES (
+    1,
+)

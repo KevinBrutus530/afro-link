742ea41c8a079b74748b41343c89cb0066d91a8c
kongnyc <bbsyoow@gmail.com>
2020-07-12 16:38:46 -0400

    adding bootrap css in index.js

--- frontend/afro-link/src/components/NewBusiness.js
+++ frontend/afro-link/src/components/NewBusiness.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useInput} from "../util/useInput"
 import {getAPI} from "../util/getAPI"
 import axios from "axios"
+import Modal from "react-bootstrap/Modal"
+import Button from 'react-bootstrap/Button';
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import ModalTitle from 'react-bootstrap/ModalTitle'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalFooter from 'react-bootstrap/ModalFooter'
 
 const NewBusiness =()=> {
 
   const API = getAPI();
+  const [modalShow, setModalShow] = React.useState(false);
 
     const biz_name = useInput("")
     const hours = useInput("")
@@ -19,29 +27,67 @@ const NewBusiness =()=> {
     const state = useInput("")
     const zip = useInput("")
     const website = useInput("")
+    
 
     const handleNewBiz = async (e)=>{
+      e.preventDefault()
       try {
-        debugger
-        let neeBiz = await axios.post(`${API}/businesses`, {
+        let newBiz = await axios.post(`${API}/businesses`, {
           biz_name: biz_name.value,
           hours: hours.value,
         });
-        console.log(neeBiz.data)
+        if(newBiz.data.status==="success"){
+          console.log(newBiz.data.payload) 
+        }
+
       } catch (error) {
-        
+        alert(error.status)
       }
-        return null
+    }
+    const HourTable= (props)=> {
+      return (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Business Hour
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>available hour</h4>
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+              consectetur ac, vestibulum at eros.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
+
+    const handleHours = (e)=>{
+      debugger
+      setModalShow(true)
     }
 
         return (
           <div>
             <form className="newBusiness" onSubmit={handleNewBiz}>
               <label>Business Name: </label>
-              <input type="text" placeholder="Business Name" {...biz_name} />
+              <input type="text" placeholder="Business Name" required {...biz_name} />
 
               <label>Hour of Service: </label>
-              <input type="text" placeholder="Business Hours" {...hours} />
+              <select onChange={()=>handleHours()} >
+                <option defaultValue="1">Online Store 24/7</option>
+                <option defaultValue="2">add businesses hours</option>
+              </select>
 
               <label>Owner Name: </label>
               <input type="text" placeholder="Owner Name" {...owner_name} />
@@ -96,6 +142,12 @@ const NewBusiness =()=> {
               <button type="submit">
                 <span>Create Business</span>
               </button>
+              <>
+                <HourTable
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                />
+              </>
             </form>
           </div>
         );

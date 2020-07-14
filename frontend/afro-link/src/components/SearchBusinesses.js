e15f3a05db3d04cf7a8d63a45965b770bf9b58dc
Kevin Brutus <kevinbrutus@pursuit.org>
2020-07-14 16:47:08 -0400

    businessRedirect function

--- frontend/afro-link/src/components/SearchBusinesses.js
+++ frontend/afro-link/src/components/SearchBusinesses.js
@@ -1,65 +1,47 @@
-import React, { useState, useEffect } from "react";
-// import {useInput} from "../util/useInput";
-import axios from "axios";
-import "../css/SearchBusinesses.css";
-
-// this app is looking to promote bob in NYC
-// users look for businesses by category
-// grab categories using axios call
-// populate the select
-// on select of category, new axios call
-// all businesses in said category populate the resultsPage
-// ***stretch*** businesses will appear in distance order
+import React, {useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom'
+import axios from 'axios';
 
 const SearchBusinessForm = () => {
-  // const location = useInput("");
-  const [category, setCategory] = useState("");
-  const [businessTypes, setBusinessTypes] = useState([]);
-  // const [search, setSearch] = useState([]);
+    const [category, setCategory] = useState("");
+    const [ businessTypes, setBusinessTypes ] = useState([]) 
+    const history = useHistory();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let res = await axios.get("http://localhost:3000/categories/");
-        setBusinessTypes(res.data.payload);
-      } catch (err) {
-        console.log(err);
-        setBusinessTypes([]);
-      }
-    };
-    fetchData();
-  }, []);
+    const businessRedirect = id => history.push(`/categories/${id}`);
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                let res = await axios.get("http://localhost:3000/categories/");
+                setBusinessTypes(res.data.payload);
+            } catch (err) {
+                console.log(err);
+                setBusinessTypes([]);
+            }
+        }
+        fetchData()
+    },[])
 
-  const handleType = (e) => {
-    setCategory(e.target.value);
-  };
+    const handleType = (e) => {
+        setCategory(e.target.value)
+    }
 
-  const types = businessTypes.map((type, i) => {
+    const types = businessTypes.map((type) => {
+        return <option value={type.id} key={type.id}>{type.type_name}</option>
+    })
+
     return (
-      <option  value={type.id} key={i}>
-        {type.type_name}
-      </option>
-    );
-  });
-
-  return (
-    <div className="selectBiz">
-      <form className="selectBizForm" onSubmit={handleSearch}>
-        <select className="selectBizBar" value={category} onChange={handleType}>
-          <option  value="">Select Business Type</option>
-          {types}
-        </select>
-        {/* <input type="text" placeholder="What you looking for?" {...location}/> */}
-        <button className="addBizBtn" id="connect" type="submit">
-          Connect
-        </button>
-      </form>
-    </div>
-  );
-};
+        <div>
+            <form onSubmit={() => businessRedirect(category)}>
+                <select value={category} onChange={handleType}>
+                <option value="">Select Business Type</option>
+                    {types}
+                </select>
+                {/* <input type="text" placeholder="What you looking for?" {...location}/> */}
+                <button type="submit">Connect</button>
+            </form>
+        </div>
+    )
+}
 
 export default SearchBusinessForm;

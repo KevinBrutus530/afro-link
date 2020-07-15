f4eb22ca547568fe60c768a8a61a7d7b62b41c69
kongnyc <bbsyoow@gmail.com>
2020-07-15 17:06:53 -0400

    try remove string quotes

--- frontend/afro-link/src/components/GoogleMap.js
+++ frontend/afro-link/src/components/GoogleMap.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import 'react-google-places-autocomplete/dist/index.min.css';
+import axios from "axios"
  
 const GoogleMap = ({setCity,setHouseNum,setStreet,setState}) => {
   console.log("hit Google")
-  // debugger
-  // window.google = window.google || {};
-  // const geocoder = new google.maps.Geocoder();
-
+  const fetchZip =async(id)=>{
+      debugger
+    try {
+      let res= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${id}
+      &key=AIzaSyB2-vWevIfqTeTiRqbV1H729BGXACYXvXU`)
+      console.log(res.data.results)
+      debugger
+  
+    } catch (error) {
+      debugger
+        console.log(error)
+    }
+    
+  }
   return(
     <div>
       <GooglePlacesAutocomplete
-      // apiKey="AIzaSyB2-vWevIfqTeTiRqbV1H729BGXACYXvXU"
-      // autocompletionRequest={(e)=>{
-      //   debugger
-      // }}
       autocompletionRequest={{
       componentRestrictions: {
         country: ['us'],
       }
     }}
         onSelect={(e)=>{
+
+          debugger
           setHouseNum(e.terms[0].value)
           setStreet(e.terms[1].value)
           setCity(e.terms[2].value)
           setState(e.terms[3].value)
+
+          fetchZip(e.reference)
+          // fetchZip(e.place_id)
           }}
-        // onSelect={console.log}
+
       />
     </div>
   )

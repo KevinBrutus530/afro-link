7b47513858dc65c29507c91a643d668e44c05050
maria liza <mariamartinez@pursuit.org>
2020-07-14 21:32:42 -0400

    get results to show up on  page

--- frontend/afro-link/src/components/Results.js
+++ frontend/afro-link/src/components/Results.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Results = () => {
   const { id } = useParams();
-  debugger;
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const getResults = async () => {
+      try {
+        let res = await axios.get(`http://localhost:3000/categories/${id}`);
+        setResults(res.data.payload);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getResults();
+  }, []);
+
+  let resultDisplay = results.map((biz, i) => {
+    return (
+      <li key={i} value={biz.biz_name}>
+        {biz.biz_name}
+      </li>
+    );
+  });
+
   return (
-    <div>
-      <h1 style={{ color: "white" }}>Businesses List</h1>
+    <div style={{ color: "white" }}>
+      <h1>Businesses List</h1>
+      <ul>{resultDisplay}</ul>
     </div>
   );
 };

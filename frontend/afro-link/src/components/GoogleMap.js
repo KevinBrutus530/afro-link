24046fe97daba86d55a2f8117a269e238f714e47
kongnyc <bbsyoow@gmail.com>
2020-07-15 22:10:48 -0400

    adding zip in new business and setup env

--- frontend/afro-link/src/components/GoogleMap.js
+++ frontend/afro-link/src/components/GoogleMap.js
@@ -1,20 +1,22 @@
+// require('dotenv').config();
 import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import 'react-google-places-autocomplete/dist/index.min.css';
-import axios from "axios"
+import axios from "axios";
+require("dotenv").config()
  
-const GoogleMap = ({setCity,setHouseNum,setStreet,setState}) => {
-  console.log("hit Google")
+const GoogleMap = ({setCity,setHouseNum,setStreet,setState, setZip}) => {
+
   const fetchZip =async(id)=>{
-      debugger
+      // debugger
     try {
-      let res= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${id}
-      &key=AIzaSyB2-vWevIfqTeTiRqbV1H729BGXACYXvXU`)
-      console.log(res.data.results)
-      debugger
-  
+      let res= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${id}`
+      +`&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
+      return setZip(res.data.results[0].address_components[7].long_name)
+      //res.data.results[0].geometry.location.lat
+      //res.data.results[0].geometry.location.lng
     } catch (error) {
-      debugger
+      // debugger
         console.log(error)
     }
     
@@ -22,6 +24,8 @@ const GoogleMap = ({setCity,setHouseNum,setStreet,setState}) => {
   return(
     <div>
       <GooglePlacesAutocomplete
+      apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+
       autocompletionRequest={{
       componentRestrictions: {
         country: ['us'],
@@ -29,14 +33,14 @@ const GoogleMap = ({setCity,setHouseNum,setStreet,setState}) => {
     }}
         onSelect={(e)=>{
 
-          debugger
+          // debugger
           setHouseNum(e.terms[0].value)
           setStreet(e.terms[1].value)
           setCity(e.terms[2].value)
           setState(e.terms[3].value)
 
           // fetchZip(e.reference)
-          // fetchZip(e.place_id)
+          fetchZip(e.place_id)
           }}
 
       />

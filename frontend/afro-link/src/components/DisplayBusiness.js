4887dd229e596c17bdbcdd3ad5be72c8622b2e35
crystal cardona <crystalcardona@pursuit.org>
2020-07-19 09:56:22 -0400

    working on displaybusiness - biz info is showing

--- frontend/afro-link/src/components/DisplayBusiness.js
+++ frontend/afro-link/src/components/DisplayBusiness.js
@@ -4,6 +4,22 @@ const DisplayBusiness = ({businessInfo}) => {
 
     return(
         <>
+                <div style={{ color: "white" }}>
+        <h1>{businessInfo.biz_name}</h1>
+        <h3>{businessInfo.owner_name}</h3>
+        <ul style={{listStyleType:"none"}}>
+        <li>{businessInfo.biz_hours}</li>
+        <li>{businessInfo.street} {businessInfo.city} {businessInfo.state} {businessInfo.zip}</li>
+        <li>{businessInfo.website}</li>
+        </ul>
+        <div className="contactInfo">
+        <ul style={{listStyleType:"none"}}>
+        <li>{businessInfo.phone}</li>
+        <li>{businessInfo.email}</li>
+        <li>{businessInfo.social_media}</li>
+        </ul>
+        </div>
+        </div>
         </>
     )
 }

281dd7d83f8bb43806cc05e40a04ce82f723d512
maria liza <mariamartinez@pursuit.org>
2020-07-21 21:58:11 -0400

    try social media function

--- frontend/afro-link/src/components/Business.js
+++ frontend/afro-link/src/components/Business.js
@@ -3,26 +3,39 @@ import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import DisplayBusiness from "./DisplayBusiness";
 import ReviewsForm from "./ReviewsForm";
-
+import Instagram from "./socialMedia/Instagram";
+import Facebook from "./socialMedia/Facebook";
 
 const Business = () => {
   const [businessInfo, setBusinessInfo] = useState([]);
   const history = useHistory();
   const { id } = useParams();
+  const [igUrl, setIgUrl] = useState("");
 
   const getInfo = async () => {
     try {
       let res = await axios.get(`http://localhost:3000/businesses/${id}`);
       setBusinessInfo(res.data.payload);
-      // debugger;
     } catch (err) {
       console.log(err);
     }
   };
+
   useEffect(() => {
     getInfo();
   }, []);
 
+  useEffect(() => {
+    checkingSocialMedia();
+  }, [businessInfo]);
+
+  const checkingSocialMedia = () => {
+    let bizInfo = businessInfo.social_media.split("");
+
+    let fb = "facebook";
+    if (bizInfo.includes(fb)) {
+    }
+  };
 
   return (
     <div>
@@ -30,10 +43,11 @@ const Business = () => {
         Return to Results Page
       </button>
       <DisplayBusiness businessInfo={businessInfo} categoryId={id} />
+      <Instagram igUrl={igUrl} />
+      {/* <Facebook fbUrl={fbUrl} /> */}
       <ReviewsForm />
     </div>
   );
 };
-
 
 export default Business;

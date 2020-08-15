f4a1a2d209be9bf931b555141376487aba054dbd
maria liza <mariamartinez@pursuit.org>
2020-08-15 13:03:16 -0400

    work on search bar

--- frontend/afro-link/src/components/Header.js
+++ frontend/afro-link/src/components/Header.js
@@ -1,9 +1,36 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink, useHistory } from "react-router-dom";
 import logo from "../images/circleLogoYellow.png";
 import "../css/Header.css";
 
 const Header = () => {
+  const history = useHistory();
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const path = () => {
+      setLocation(history.location.pathname);
+    };
+    path();
+    console.log("header is showing up");
+    searchBiz();
+  }, [location]);
+
+  const searchBiz = () => {
+    console.log("checking");
+    return (
+      <div>
+        <form>
+          <label>
+            Search
+            <input type="text" placeholder="Search Businesses" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  };
+
   return (
     <div>
       <nav className="headerNav">
@@ -13,6 +40,7 @@ const Header = () => {
             <img className="logoHeader" src={logo} alt="logo" />
           </NavLink>
         </p>
+        {searchBiz}
       </nav>
     </div>
   );

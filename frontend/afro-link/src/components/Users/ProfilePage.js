1ca98227040444047cfd44254431dc3bb6fc6151
maria liza <mariamartinez@pursuit.org>
2020-08-22 12:34:05 -0400

    correct getsingle owner route and queries

--- frontend/afro-link/src/components/Users/ProfilePage.js
+++ frontend/afro-link/src/components/Users/ProfilePage.js
@@ -1,44 +1,37 @@
-import React, { useContext, useEffect } from 'react';
-import axios from 'axios';
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 import { getAPI } from "../../util/getAPI";
-import { AuthContext } from "../../providers/AuthContext"
-import { logout } from "../../util/firebaseFunctions"
+import { AuthContext } from "../../providers/AuthContext";
+import { logout } from "../../util/firebaseFunctions";
 
 const ProfilePage = () => {
-    let API = getAPI();
-    const {
-        token,
-        currentUser,
-        loading
-    } = useContext(AuthContext)
+  let API = getAPI();
+  const { token, currentUser, loading } = useContext(AuthContext);
 
-    useEffect(()=> {
-        const fetchUserById = async () => {
-            let res = await axios({
-              method: "get",
-              url: `${API}/owners/${currentUser.uid}`,
-              headers: {
-                AuthToken: token
-              }
-            });
-            console.log(currentUser.uid, "current uid");
-            // setUser(res.data.user);
-            console.log(res.data.user);
-          };
-          fetchUserById();
-      
-      
-    }, [])
+  console.log(currentUser);
 
-    return (
+  useEffect(() => {
+    const fetchUserById = async () => {
+      let res = await axios({
+        method: "get",
+        url: `${API}/owners/${currentUser.uid}`,
+        headers: {
+          AuthToken: token,
+        },
+      });
+      console.log(currentUser.uid, "current uid");
+      // setUser(res.data.user);
+      console.log(res.data.user);
+    };
+    fetchUserById();
+  }, []);
+
+  return (
     <div>
-        <h1 style={{ "padding" : "2em", "color" : "white" }}> Profile Page</h1>
-        <button onClick={logout}>
-        Log Out
-        </button>
-
+      <h1 style={{ padding: "2em", color: "white" }}> Profile Page</h1>
+      <button onClick={logout}>Log Out</button>
     </div>
-    )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;

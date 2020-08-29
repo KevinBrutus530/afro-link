3a62d4f55da4931ec0be95a6003fb3c95120bfd2
maria liza <mariamartinez@pursuit.org>
2020-08-29 00:34:25 -0400

    more styling changes

--- frontend/afro-link/src/components/MainPage.js
+++ frontend/afro-link/src/components/MainPage.js
@@ -100,8 +100,8 @@ const MainPage = () => {
         </div>
       </div>
       <div className="ourMissionStatementDiv">
-      <div className="logoDiv"><img id="alLogo" src={circleLogo} alt="afrolink" /></div>
-        <h1 className="heavyFont mission">Our Mission</h1>
+      {/* <div className="logoDiv"><img id="alLogo" src={circleLogo} alt="afrolink" /></div> */}
+        <h1 className="heavyFont">Our Mission</h1>
         <p className="alMission">
           We created this site to honor and celebrate Black entrepreneurship by
           highlighting authentic Black owned businesses and services right in

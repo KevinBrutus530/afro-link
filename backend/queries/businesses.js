4be3a0b1ac128df016671bba28d0b2ccad5cfade
Kevin Brutus <kevinbrutus@pursuit.org>
2020-08-29 11:50:42 -0400

    fix editBusiness query

--- backend/queries/businesses.js
+++ backend/queries/businesses.js
@@ -1,19 +1,19 @@
-const db = require("../db/index");
+const db = require('../db/index');
 
 const getAllBusiness = async (req, res, next) => {
   try {
     let business = await db.any(
-      "SELECT businesses.id, businesses.biz_name, businesses.hours, addresses.street, addresses.city, addresses.state, addresses.zip, addresses.website, contacts.phone, contacts.email, contacts.social_media, types.type_name, owners.owner_name FROM contacts RIGHT JOIN addresses ON addresses.address_id=contact_id JOIN owners ON owners.owner_id=addresses.address_id JOIN businesses  ON businesses.id=owners.owner_id JOIN categories ON categories.biz_id=addresses.address_id JOIN types ON types.id=categories.type_id"
+      'SELECT businesses.id, businesses.biz_name, businesses.hours, addresses.street, addresses.city, addresses.state, addresses.zip, addresses.website, contacts.phone, contacts.email, contacts.social_media, types.type_name, owners.owner_name FROM contacts RIGHT JOIN addresses ON addresses.address_id=contact_id JOIN owners ON owners.owner_id=addresses.address_id JOIN businesses  ON businesses.id=owners.owner_id JOIN categories ON categories.biz_id=addresses.address_id JOIN types ON types.id=categories.type_id'
     );
     res.status(200).json({
-      status: "success",
-      message: "all businesses",
+      status: 'success',
+      message: 'all businesses',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error get all business",
+      status: 'Error',
+      message: 'Error get all business',
       payload: err,
     });
     next();
@@ -22,17 +22,17 @@ const getAllBusiness = async (req, res, next) => {
 const getAllBusinessByOwner = async (req, res, next) => {
   try {
     let business = await db.any(
-      "SELECT businesses.id, businesses.biz_name, businesses.hours, addresses.street, addresses.city, addresses.state, addresses.zip, addresses.website, contacts.phone, contacts.email, contacts.social_media, types.type_name, owners.owner_name FROM contacts RIGHT JOIN addresses ON addresses.address_id=contact_id JOIN owners ON owners.owner_id=addresses.address_id JOIN businesses  ON businesses.id=owners.owner_id JOIN categories ON categories.biz_id=addresses.address_id JOIN types ON types.id=categories.type_id"
+      'SELECT businesses.id, businesses.biz_name, businesses.hours, addresses.street, addresses.city, addresses.state, addresses.zip, addresses.website, contacts.phone, contacts.email, contacts.social_media, types.type_name, owners.owner_name FROM contacts RIGHT JOIN addresses ON addresses.address_id=contact_id JOIN owners ON owners.owner_id=addresses.address_id JOIN businesses  ON businesses.id=owners.owner_id JOIN categories ON categories.biz_id=addresses.address_id JOIN types ON types.id=categories.type_id'
     );
     res.status(200).json({
-      status: "success",
-      message: "all businesses by Owner",
+      status: 'success',
+      message: 'all businesses by Owner',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error get all business by Owner",
+      status: 'Error',
+      message: 'Error get all business by Owner',
       payload: err,
     });
     next();
@@ -45,13 +45,13 @@ const getSingleBusiness = async (req, res, next) => {
       `SELECT businesses.id, businesses.biz_name, businesses.hours, addresses.street, addresses.city, addresses.state, addresses.zip, addresses.website, contacts.phone, contacts.email, contacts.social_media, types.type_name, owners.owner_name, owners.pictures FROM contacts RIGHT JOIN addresses ON addresses.address_id=contact_id JOIN owners ON owners.owner_id=addresses.address_id JOIN businesses  ON businesses.id=owners.owner_id JOIN categories ON categories.biz_id=addresses.address_id JOIN types ON types.id=categories.type_id WHERE businesses.id =${req.params.id}`
     );
     res.status(200).json({
-      status: "success",
-      message: "single business",
+      status: 'success',
+      message: 'single business',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
+      status: 'Error',
       message: "Couldn't get single business",
       payload: err,
     });
@@ -62,15 +62,15 @@ const getSingleBusiness = async (req, res, next) => {
 const deleteBusiness = async (req, res, next) => {
   try {
     let { id } = req.params;
-    await db.none("DELETE FROM businesses WHERE id=$1", id);
+    await db.none('DELETE FROM businesses WHERE id=$1', id);
     res.status(200).json({
-      status: "success",
-      message: "deleted business",
+      status: 'success',
+      message: 'deleted business',
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error deleting business",
+      status: 'Error',
+      message: 'Error deleting business',
       payload: err,
     });
     next();
@@ -89,14 +89,14 @@ const getSearchForBusiness = async (req, res, next) => {
       ORDER BY biz_name ASC`
     );
     res.status(200).json({
-      status: "success",
-      message: "found businesses",
+      status: 'success',
+      message: 'found businesses',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error get search Business",
+      status: 'Error',
+      message: 'Error get search Business',
       payload: err,
     });
     next();
@@ -107,18 +107,18 @@ const createBusiness = async (req, res, next) => {
   try {
     let { biz_name, hours } = req.body;
     let business = await db.one(
-      "INSERT INTO businesses (biz_name, hours) VALUES ($1,$2) RETURNING *",
+      'INSERT INTO businesses (biz_name, hours) VALUES ($1,$2) RETURNING *',
       [biz_name, hours]
     );
     res.status(200).json({
-      status: "success",
-      message: "added business",
+      status: 'success',
+      message: 'added business',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error create business",
+      status: 'Error',
+      message: 'Error create business',
       payload: err,
     });
     next();
@@ -130,17 +130,17 @@ const editBusiness = async (req, res, next) => {
     let { biz_name, hours } = req.body;
     let { id } = req.params;
     let business = await db.one(
-      `UPDATE businesses SET biz_name='${biz_name}', hours='${hours}' WHERE id='${id}'`
+      `UPDATE businesses SET biz_name='${biz_name}', hours='${hours}' WHERE id='${id}' RETURNING *`
     );
     res.status(200).json({
-      status: "success",
-      message: "updated business",
+      status: 'success',
+      message: 'updated business',
       payload: business,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Error",
-      message: "Error for update business",
+      status: 'Error',
+      message: 'Error for update business',
       payload: err,
     });
     next();
@@ -154,5 +154,5 @@ module.exports = {
   createBusiness,
   editBusiness,
   deleteBusiness,
-  getAllBusinessByOwner
+  getAllBusinessByOwner,
 };

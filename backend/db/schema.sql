255ff05602aae3d7e98d4b9526334d7aca102f7d
Kevin Brutus <kevinbrutus@pursuit.org>
2020-09-08 21:02:00 -0400

    set default photo for businesses

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -8,32 +8,34 @@ DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS types;
 DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS businesses;
-CREATE TABLE businesses  
+CREATE TABLE businesses
 (
   id serial PRIMARY KEY,
   biz_name varchar,
   hours varchar
 );
-CREATE TABLE owners 
+CREATE TABLE owners
 (
   id serial PRIMARY KEY,
   user_id VARCHAR,
   email VARCHAR,
   owner_id int REFERENCES businesses(id) ON DELETE CASCADE,
   owner_name varchar DEFAULT 'UNKNOWN',
-  pictures varchar DEFAULT NULL
+  pictures varchar DEFAULT 'https://trello-attachments.s3.amazonaws.com/5ef8bcf3775f24613fe56eb7/5ef8bfd9edb57a1eb08bbe57/9306e0b1aa785ac7b6df7e1b16eff0f1/circleLogoYellow.png'
 );
-CREATE TABLE types (
-    id SERIAL PRIMARY KEY,
-    type_name VARCHAR(100)
+CREATE TABLE types
+(
+  id SERIAL PRIMARY KEY,
+  type_name VARCHAR(100)
 );
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    biz_id INT REFERENCES businesses(id) ON DELETE CASCADE,
-    type_id INT REFERENCES types(id),
-    CONSTRAINT UC_Categor UNIQUE (biz_id, type_id)
+CREATE TABLE categories
+(
+  id SERIAL PRIMARY KEY,
+  biz_id INT REFERENCES businesses(id) ON DELETE CASCADE,
+  type_id INT REFERENCES types(id),
+  CONSTRAINT UC_Categor UNIQUE (biz_id, type_id)
 );
-CREATE TABLE contacts 
+CREATE TABLE contacts
 (
   id serial PRIMARY KEY,
   contact_id int REFERENCES businesses(id) ON DELETE CASCADE,
@@ -41,7 +43,7 @@ CREATE TABLE contacts
   email varchar,
   social_media varchar
 );
-CREATE TABLE addresses 
+CREATE TABLE addresses
 (
   id serial PRIMARY KEY,
   address_id int REFERENCES businesses(id) ON DELETE CASCADE,
@@ -50,8 +52,8 @@ CREATE TABLE addresses
   state varchar,
   zip int,
   website varchar
- );
-CREATE TABLE reviews 
+);
+CREATE TABLE reviews
 (
   id serial PRIMARY KEY,
   dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -62,5 +64,5 @@ CREATE TABLE reviews
   reply int DEFAULT NULL,
   reply_id int REFERENCES reviews(id),
   reply_text VARCHAR
- );
+);
 

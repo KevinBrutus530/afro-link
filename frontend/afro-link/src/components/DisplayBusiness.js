a95148436fcde89b626fff7ad0e4744999471adb
maria liza <mariamartinez@pursuit.org>
2020-09-10 14:31:18 -0400

    edit and fix business info on business page, including icons

--- frontend/afro-link/src/components/DisplayBusiness.js
+++ frontend/afro-link/src/components/DisplayBusiness.js
@@ -1,21 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../css/DisplayBusiness.css';
+import browserIcon from '../images/browserIcon.png';
+import emailIcon from '../images/emailIcon.png';
+import fbIcon from '../images/facebookIcon.png';
+import locationIcon from '../images/locationIcon.png';
+import phoneIcon from '../images/phoneIcon.png';
+import twitterIcon from '../images/twitterIcon.png';
+import igIcon from '../images/instagramIcon.png';
 
 const DisplayBusiness = ({ businessInfo }) => {
-  // debugger
   const [bizNezHours, setBizNezHours] = useState('');
   const history = useHistory();
 
+  console.log(businessInfo);
+
   const checkingSocialMedia = (url) => {
+    //fn replaces word link with icon link for social media
     let fb = 'facebook';
     let ins = 'instagram';
+    let tw = 'twitter';
 
-    if (!url) return null;
+    if (!url) return '';
     if (url.includes(ins)) {
-      return <p>instagram</p>;
+      return (
+        <a href={businessInfo.social_media} target="_blank">
+          <img
+            style={{ width: '30px', height: '30px' }}
+            src={igIcon}
+            alt={'instagram icon'}
+          />
+        </a>
+      );
     } else if (url.includes(fb)) {
-      return <p>facebook</p>;
+      return (
+        <a href={businessInfo.social_media} target="_blank">
+          <img
+            style={{ width: '30px', height: '30px' }}
+            src={fbIcon}
+            alt={'facebook icon'}
+          />
+        </a>
+      );
+    } else if (url.includes(tw)) {
+      return (
+        <a href={businessInfo.social_media} target="_blank">
+          <img
+            style={{ width: '30px', height: '30px' }}
+            src={twitterIcon}
+            alt={'twitter icon'}
+          />
+        </a>
+      );
     } else {
       return <p>{url}</p>;
     }
@@ -27,7 +63,7 @@ const DisplayBusiness = ({ businessInfo }) => {
       : businessInfo.owner_name;
 
   return (
-    <div className="businessDisplay" style={{ color: 'white' }}>
+    <div className="businessDisplay" style={{ color: 'whiteSmoke' }}>
       <div className="ownerHeader">
         <h3 className="heavyFont">{owner}</h3>
         {owner === '' ? (
@@ -37,48 +73,90 @@ const DisplayBusiness = ({ businessInfo }) => {
         )}
       </div>
       <div className="businessInfo">
-        <ul style={{ listStyleType: 'none' }}>
+        <ul>
           <li>
             <img className="bizPicProfile" src={businessInfo.pictures} />
           </li>
+
           <li className="bizAddress">
             {businessInfo.street} {businessInfo.city} {businessInfo.state}{' '}
             {businessInfo.zip}
           </li>
-          <label className="bizLabel">
-            Business Hours:
-            <li className="bizHoursProfile">
-              {businessInfo.hours === 'Online Store' ? (
-                <p>Online Business</p>
+
+          <div className="contactInfo">
+            <label className="bizLabel">
+              Business Hours:
+              <li className="bizHoursProfile">
+                {businessInfo.hours === 'Online Store' ? (
+                  <p>Online Business</p>
+                ) : (
+                  <details className="hoursDetails">
+                    <summary>Hours</summary>
+                    {businessInfo.hours}
+                  </details>
+                )}
+              </li>
+            </label>
+
+            {/* if no phone number */}
+            <label className="bizLabel">
+              <img
+                src={phoneIcon}
+                alt={'phone icon'}
+                style={{ width: '30px', height: '30px' }}
+              />
+              {businessInfo.phone !== (null || 'n/a' || '') ? (
+                <li>{businessInfo.phone}</li>
               ) : (
-                <details className="hoursDetails">
-                  <summary>Hours</summary>
-                  {businessInfo.hours}
-                </details>
+                <li>None Available</li>
               )}
-            </li>
-          </label>
-          {/* <li className="bizHours">{businessInfo.hours}</li> */}
+            </label>
 
-          <li className="hyperLink">
-            <a href={businessInfo.website} target="_blank">
-              Visit Website
-            </a>
-          </li>
-        </ul>
-        <div className="contactInfo">
-          <ul>
-            <label id="contactsLabel">Contact Info: </label>
-            <li>{businessInfo.phone}</li>
-            <li>{businessInfo.email}</li>
+            {/* if no email */}
+            <label className="bizLabel">
+              <img
+                src={emailIcon}
+                alt={'email icon'}
+                style={{ width: '30px', height: '30px' }}
+              />
+              {businessInfo.email !== (null || '') ? (
+                <li>{businessInfo.email}</li>
+              ) : (
+                <li>None Available</li>
+              )}
+            </label>
 
-            <li className="hyperLink">
-              <a href={businessInfo.social_media} target="_blank">
-                {checkingSocialMedia(businessInfo.social_media)}
+            {/* if no website avaiable */}
+            <label className="bizLabel">
+              <a href={businessInfo.website} target="_blank">
+                <img
+                  style={{ width: '30px', height: '30px' }}
+                  src={browserIcon}
+                  alt={'website'}
+                />
               </a>
-            </li>
-          </ul>
-        </div>
+              {businessInfo.website !== (null || '') ? (
+                <li className="hyperLink smallertxt">{businessInfo.website}</li>
+              ) : (
+                <li>None Available</li>
+              )}
+            </label>
+
+            {/* if no social media */}
+            {businessInfo.social_media !== null || 'n/a' || '' ? (
+              <label className="bizLabel">
+                {checkingSocialMedia(businessInfo.social_media)}
+                <li className="hyperLink smallertxt">
+                  <a href={businessInfo.social_media} target="_blank">
+                    {businessInfo.social_media}
+                  </a>
+                </li>
+              </label>
+            ) : (
+              ''
+            )}
+          </div>
+        </ul>
       </div>
     </div>
   );

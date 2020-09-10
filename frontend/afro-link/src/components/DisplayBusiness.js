83c4e11e0a01b51e08b0bd4bca9171a6a2041968
maria liza <mariamartinez@pursuit.org>
2020-09-10 11:02:52 -0400

    work on business page styling

--- frontend/afro-link/src/components/DisplayBusiness.js
+++ frontend/afro-link/src/components/DisplayBusiness.js
@@ -27,62 +27,60 @@ const DisplayBusiness = ({ businessInfo }) => {
       : businessInfo.owner_name;
 
   return (
-    <>
-      <div className="businessDisplay" style={{ color: 'white' }}>
-        <div className="ownerHeader">
-          <h3 className="heavyFont">{owner}</h3>
-          {owner === '' ? (
-            <p className="empty"></p>
-          ) : (
-            <p className="lightGrey">Owner/Operator</p>
-          )}
-        </div>
-        <div className="businessInfo">
-          <ul style={{ listStyleType: 'none' }}>
-            <li>
-              <img className="bizPicProfile" src={businessInfo.pictures} />
+    <div className="businessDisplay" style={{ color: 'white' }}>
+      <div className="ownerHeader">
+        <h3 className="heavyFont">{owner}</h3>
+        {owner === '' ? (
+          <p className="empty"></p>
+        ) : (
+          <p className="lightGrey">Owner/Operator</p>
+        )}
+      </div>
+      <div className="businessInfo">
+        <ul style={{ listStyleType: 'none' }}>
+          <li>
+            <img className="bizPicProfile" src={businessInfo.pictures} />
+          </li>
+          <li className="bizAddress">
+            {businessInfo.street} {businessInfo.city} {businessInfo.state}{' '}
+            {businessInfo.zip}
+          </li>
+          <label className="bizLabel">
+            Business Hours:
+            <li className="bizHoursProfile">
+              {businessInfo.hours === 'Online Store' ? (
+                <p>Online Business</p>
+              ) : (
+                <details className="hoursDetails">
+                  <summary>Hours</summary>
+                  {businessInfo.hours}
+                </details>
+              )}
             </li>
-            <li className="bizAddress">
-              {businessInfo.street} {businessInfo.city} {businessInfo.state}{' '}
-              {businessInfo.zip}
-            </li>
-            <label className="bizLabel">
-              Business Hours:
-              <li className="bizHoursProfile">
-                {businessInfo.hours === 'Online Store' ? (
-                  <p>Online Business</p>
-                ) : (
-                  <details className="hoursDetails">
-                    <summary>Hours</summary>
-                    {businessInfo.hours}
-                  </details>
-                )}
-              </li>
-            </label>
-            {/* <li className="bizHours">{businessInfo.hours}</li> */}
+          </label>
+          {/* <li className="bizHours">{businessInfo.hours}</li> */}
+
+          <li className="hyperLink">
+            <a href={businessInfo.website} target="_blank">
+              Visit Website
+            </a>
+          </li>
+        </ul>
+        <div className="contactInfo">
+          <ul>
+            <label id="contactsLabel">Contact Info: </label>
+            <li>{businessInfo.phone}</li>
+            <li>{businessInfo.email}</li>
 
             <li className="hyperLink">
-              <a href={businessInfo.website} target="_blank">
-                Visit Website
+              <a href={businessInfo.social_media} target="_blank">
+                {checkingSocialMedia(businessInfo.social_media)}
               </a>
             </li>
           </ul>
-          <div className="contactInfo">
-            <ul>
-              <label id="contactsLabel">Contact Info: </label>
-              <li>{businessInfo.phone}</li>
-              <li>{businessInfo.email}</li>
-
-              <li className="hyperLink">
-                <a href={businessInfo.social_media} target="_blank">
-                  {checkingSocialMedia(businessInfo.social_media)}
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

aaaef74e6ae53383d3beb906a2640cb2236d037c
Kevin Brutus <kevinbrutus@pursuit.org>
2020-09-12 15:15:28 -0400

    deploying backend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
-const port = 3005;
+const port = process.env.PORT || 3005;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,3 +1,4 @@
 const pgp = require('pg-promise')({});
-const db = pgp("postgress://localhost:5432/afro_link");
+require("dotenv").config()
+const db = pgp(process.env.DATABASE_URL);
 module.exports = db;

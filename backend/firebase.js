2f019da817fe95c7920eae35f74b08a6d15af5b2
Kevin Brutus <kevinbrutus@pursuit.org>
2020-09-13 12:54:50 -0400

    fix firebase

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,10 +1,10 @@
-const admin = require("firebase-admin");
-require("dotenv").config();
+const admin = require('firebase-admin');
+require('dotenv').config();
 const serviceAccount = {
   type: process.env.PROJECT_ID,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
@@ -13,7 +13,7 @@ const serviceAccount = {
   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
 };
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://afrolink-37a63.firebaseio.com"
-  });
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://afrolink-37a63.firebaseio.com',
+});
 module.exports = admin;

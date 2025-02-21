2664f48b9b3ddaee6c71db7623376eb874be8c64
Kevin Brutus <kevinbrutus@pursuit.org>
2025-02-21 18:23:55 -0500

    Fix database connection with SSL

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,4 +1,10 @@
 const pgp = require('pg-promise')({});
 require("dotenv").config();
-const db = pgp(process.env.DATABASE_URL);
+// const db = pgp(process.env.DATABASE_URL);
+const db = pgp({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // Required for Heroku Postgres
+    },
+  });
 module.exports = db;

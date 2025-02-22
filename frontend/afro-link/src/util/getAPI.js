8a26db156ce5532d2c784f349352fdcecb0b2b3a
Kevin Brutus <kevinbrutus@pursuit.org>
2025-02-21 19:38:48 -0500

    adjust .env

--- frontend/afro-link/src/util/getAPI.js
+++ frontend/afro-link/src/util/getAPI.js
@@ -1,6 +1,6 @@
 export const getAPI = () => {
-  // return window.location.hostname === 'localhost'
-  //   ? 'http://localhost:3005'
-  //   : 'https://afro-link-db.herokuapp.com';
-  return 'http://localhost:3005';
+  return window.location.hostname === 'localhost'
+    ? 'http://localhost:3005'
+    : 'https://afro-link-db.herokuapp.com';
+    // return 'https://afro-link-db.herokuapp.com';
 };
